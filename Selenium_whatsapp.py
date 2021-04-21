3cdadaf0c338ccb261539be1301a3af4f0831d38
Anas Bin Faisal <anasbinfaisal@gmail.com>
2021-04-21 17:56:21 +0500

    Update Selenium_whatsapp.py

--- Selenium_whatsapp.py
+++ Selenium_whatsapp.py
@@ -6,22 +6,22 @@ from selenium.webdriver.common.keys import  Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver.get("https://web.whatsapp.com/")
+driver.get("https://web.whatsapp.com/") 
 print("Scan QR code and Press enter")
 input()
 print("Logged In")
 time.sleep(5)
-contact = 'Mum'
-mssg = 'Whatsapp TEST Message!!!!'
+contact = 'Contact_Name' //contact to be sent a message
+mssg = 'Whatsapp TEST Message!!!!' //message
 
-
-search_box = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
-search_box.send_keys(contact)
-search_box.send_keys(Keys.ENTER)
+//Search Box 
+search_box = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]') //Search box xpath
+search_box.send_keys(contact) //enter contact to be searched in search box
+search_box.send_keys(Keys.ENTER) ////pressing enter after entering the contact 
 time.sleep(5)
-for i in range(30):
-    mssg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    mssg_box.send_keys(random.choice(mssg))
-    mssg_box.send_keys(Keys.ENTER)
+for i in range(30):     //To Send 30 Messages
+    mssg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]') //Message Box Xpath
+    mssg_box.send_keys(mssg) //Message to be sent
+    mssg_box.send_keys(Keys.ENTER) //pressing enter after entering the message
 time.sleep(100)
 driver.quit()
